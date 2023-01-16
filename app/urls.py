3c9f6150d2674367455da3f85ca4fdd20cb48348
RajrupDasid <rajrupdasid@outlook.com>
2023-01-16 20:47:55 +0530

    detail view added

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView,BlogDetailView
 urlpatterns = [
  path("",IndexView.as_view(),name="indexpage"),
+ path("detail/<slug:slug>",BlogDetailView.as_view(),name="article-detail")
 ]
