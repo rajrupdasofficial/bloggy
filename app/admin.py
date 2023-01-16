0d338c5ea0eae65488fa285bfd0a6d1cfbecd385
RajrupDasid <rajrupdasid@outlook.com>
2023-01-16 20:56:48 +0530

    admin file patched

--- app/admin.py
+++ app/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from .models import Blog
 # Register your models here.
-#@admin.register(Blog)
-#class PostAdmin(admin.ModelAdmin):
-#    class Media:
-#        js = ('static/js/uploader.js')
-
-
-admin.site.register(Blog)
-#admin.site.register(PostAdmin)
+@admin.register(Blog)
+class PostAdmin(admin.ModelAdmin):
+    exclude = ['slug']
+    list_display = ('title','created','updated',)
+    list_display_links = ('title',)
+    list_per_page = 30
