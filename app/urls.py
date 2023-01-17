c2869d21634e1d90a0c5bee7c451a3f81dcd2c73
RajrupDasid <rajrupdasid@outlook.com>
2023-01-17 21:55:17 +0530

    url updated with details, gallery,and updated index view

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView,BlogDetailView
+from .views import IndexView,BlogDetailView,GalleryView,index
 urlpatterns = [
- path("",IndexView.as_view(),name="indexpage"),
- path("detail/<slug:slug>",BlogDetailView.as_view(),name="article-detail")
+ path("",index,name="indexpage"),
+ path("<slug:slug>/",BlogDetailView.as_view(),name="article-detail"),
+ path("m",GalleryView.as_view(),name="gallery")
 ]
