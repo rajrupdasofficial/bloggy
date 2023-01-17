65f3405196aa22a42981cbb7a732dc110e46eadf
RajrupDasid <rajrupdasid@outlook.com>
2023-01-17 22:01:11 +0530

    main url view sets updated and with hidden admin path

--- mrblog/urls.py
+++ mrblog/urls.py
@@ -17,11 +17,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from decouple import config
 urlpatterns = [
     path("",include('app.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('admin/', admin.site.urls),
+    path(config('admin'), admin.site.urls),
   
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
