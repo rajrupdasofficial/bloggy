195c3f4f147b833eb0fdf40d1f1ed25c22a8d2d2
RajrupDasid <rajrupdasid@outlook.com>
2023-01-18 21:15:38 +0530

    linting rectified

--- app/models.py
+++ app/models.py
@@ -11,10 +11,11 @@ from ckeditor_uploader.fields import RichTextUploadingField
 
 
 class Analytics(models.Model):
-    ip = models.CharField(max_length=255,null=True,blank=True,default=None)
+    ip = models.CharField(max_length=255, null=True, blank=True, default=None)
 
     def __str__(self) -> str:
         return f"{self.ip}"
+
     class Meta:
         verbose_name = 'Analytic'
         verbose_name_plural = 'Analytics'
