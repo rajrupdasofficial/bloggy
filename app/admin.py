519cef9eba3214e049e9978b4718a019f46102f0
RajrupDasid <rajrupdasid@outlook.com>
2023-01-18 16:17:35 +0530

    rectified syntax using flake8

--- app/admin.py
+++ app/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from .models import Blog,Analytics
+from .models import Blog, Analytics
 # Register your models here.
+
+
 @admin.register(Blog)
 class PostAdmin(admin.ModelAdmin):
     exclude = ['slug']
-    list_display = ('title','created','updated',)
+    list_display = ('title', 'created', 'updated',)
     list_display_links = ('title',)
     list_per_page = 30
+
+
 @admin.register(Analytics)
 class AnalyticsAdmin(admin.ModelAdmin):
-    list_per_page=30
+    list_per_page = 30
