77f7f5bbdaed39e6a470bfa75addfc2b0de05103
RajrupDasid <rajrupdasid@outlook.com>
2023-01-19 20:48:40 +0530

    comment and about view section added with some logic fix

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Blog, Analytics, Comment
+from .models import Blog, Analytics, Comment, Contact
 from django.core.paginator import Paginator
 from gallery.models import Photo
 from django.contrib import messages
@@ -23,6 +23,8 @@ def index(request):
             'page_obj': page_obj,
         }
         return render(request, 'index.html', context)
+    else:
+        messages.error(request, "Something went wrong please try again")
 
 
 def blogdetail(request, slug):
@@ -72,3 +74,40 @@ def galleryview(request):
             'page_obj': page_obj
         }
         return render(request, 'gallery.html', context)
+    else:
+        messages.error(request, "Something went wrong please try again")
+
+
+def contactview(request):
+    if request.method == "GET":
+        x_forw_for = request.META.get('HTTP_X_FORWARDED_F0R')
+        if x_forw_for is not None:
+            ip = x_forw_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+            analytics = Analytics(ip=ip)
+            analytics.save()
+        return render(request, 'contact.html')
+    elif request.method == "POST":
+        name = request.POST["name"]
+        email = request.POST["email"]
+        subject = request.POST["subject"]
+        message = request.POST["message"]
+        if len(name) < 5 or len(email) < 5 and len(subject) < 5 or len(message) < 5:
+            messages.error(
+                request,
+                "Your form contains error please give a name , email , subject , message at least 5 character and try again")
+        else:
+            contact = Contact(name=name, email=email, subject=subject, message=message)
+            contact.save()
+            messages.success(request, "The data you have provided has been saved successfully")
+        return render(request, "contact.html")
+    else:
+        messages.error(request, "Something went wrong please try again")
+
+
+def aboutview(request):
+    if request.method == "GET":
+        return render(request, "about.html")
+    else:
+        messages.error(request, "Something went wrong please try again")
