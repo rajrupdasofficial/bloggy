818710ac378680e97d91be8d7a70ca9ae9a7c29f
RajrupDasid <rajrupdasid@outlook.com>
2023-01-19 15:07:58 +0530

    page obj

--- app/views.py
+++ app/views.py
@@ -20,7 +20,7 @@ def index(request):
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
         context = {
-            'page_obj': page_obj
+            'page_obj': page_obj,
         }
         return render(request, 'index.html', context)
 
