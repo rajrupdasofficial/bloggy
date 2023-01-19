c39d5e534c024b297e730a4c8e14f4d785b9f626
RajrupDasid <rajrupdasid@outlook.com>
2023-01-19 13:39:43 +0530

    Comment model object added | pep8 styled

--- app/models.py
+++ app/models.py
@@ -73,6 +73,17 @@ class Blog(models.Model):
         return reverse('article-detail', kwargs={'slug': self.slug})
 
 
+class Comment(models.Model):
+    name = models.CharField(max_length=255, default=None, blank=True, null=True)
+    email = models.EmailField(max_length=255, default=None, blank=True, null=True)
+    comment = models.TextField(null=True, blank=True, default=None)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"comment author name {self.name}, comment author email {self.email}"
+
+
 class TestModel(models.Model):
     name = models.CharField(max_length=255)
     bio = models.CharField(max_length=255)
