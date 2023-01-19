594344a76a0a739b5b6929eb03f18dd5ee9c72f6
RajrupDasid <rajrupdasid@outlook.com>
2023-01-19 20:48:05 +0530

    models are activated and comment models added

--- app/models.py
+++ app/models.py
@@ -91,6 +91,21 @@ class Comment(models.Model):
         ordering = ['-created']
 
 
+class Contact(models.Model):
+    name = models.CharField(max_length=255, default=None, blank=True, null=True)
+    email = models.EmailField(max_length=255, default=None, blank=True, null=True)
+    subject = models.CharField(max_length=255, default=None, blank=True, null=True)
+    message = models.TextField(default=None, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"contact name {self.name}, contact name {self.email}"
+
+    class Meta:
+        ordering = ['-created']
+
+
 class TestModel(models.Model):
     name = models.CharField(max_length=255)
     bio = models.CharField(max_length=255)
