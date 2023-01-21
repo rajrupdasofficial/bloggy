b44a0e66b937d6ada069acaecd5e3c40c12f11e6
RajrupDasid <rajrupdasid@outlook.com>
2023-01-21 20:13:39 +0530

    video watch url added

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index, blogdetail, galleryview, contactview, aboutview
+from .views import index, blogdetail, galleryview, contactview, aboutview, watchview
 
 urlpatterns = [
     path("", index, name="indexpage"),
     path("<slug:slug>.html", blogdetail, name="article-detail"),
     path("artgallery", galleryview, name="gallery"),
     path("contact", contactview, name="contact"),
-    path("about", aboutview, name="about")
+    path("about", aboutview, name="about"),
+    path("watch",watchview,name="videos"),
 ]
