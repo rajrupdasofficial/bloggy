fdc4a7f7e22a4d39b4114b61b506847eb30fbd23
RajrupDasid <rajrupdasid@outlook.com>
2023-01-22 19:04:03 +0530

    cache middle ware added

--- mrblog/settings.py
+++ mrblog/settings.py
@@ -49,17 +49,17 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'django.middleware.cache.UpdateCacheMiddleware',
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.cache.FetchFromCacheMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-# CACHE_MIDDLEWARE_ALIAS="default"
-# CACHE_MIDDLEWARE_SECONDS=6
-# CACHE_MIDDLEWARE_KEY_PREFIX="default"
+CACHE_MIDDLEWARE_ALIAS = "default"
+CACHE_MIDDLEWARE_SECONDS = 600
+CACHE_MIDDLEWARE_KEY_PREFIX = "default"
 
 ROOT_URLCONF = 'mrblog.urls'
 
