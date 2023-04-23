2b28c9889c19b43fbf017da65f69aa214667b8b4
RajrupDasid <rajrupdasid@keemail.me>
2023-04-23 19:22:30 +0530

    massive modification

--- mrblog/settings.py
+++ mrblog/settings.py
@@ -81,7 +81,7 @@ ROOT_URLCONF = 'mrblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
