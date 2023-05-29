a3f33c092263a0bb2d259467ae174d125e6d993e
RajrupDasid <rajrupdasid@keemail.me>
2023-05-29 22:07:35 +0530

    #9 glitch fixes done added proper module logics

--- videoapp/admin.py
+++ videoapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from .models import  VideoUpload,VideoFileDetails
 # Register your models here.
+
+class VideoAdmin(admin.StackedInline):
+    model = VideoUpload
+    exclude = ["slug",]
+
+
+@admin.register(VideoFileDetails)
+class VideoDetailsAdmin(admin.ModelAdmin):
+    inlines = [VideoAdmin]
+    list_display = ('name', 'created',)
+    list_display_links = ('name',)
+    list_per_page = 30
+
+
+@admin.register(VideoUpload)
+class VideoUploadAdmin(admin.ModelAdmin):
+    exclude = ["slug",]
+    list_per_page = 30
