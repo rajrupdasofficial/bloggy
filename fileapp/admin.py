a9ff1795ecd56bfe4ca49d6552e9ee72a1a65ea1
RajrupDasid <rajrupdasid@keemail.me>
2023-05-29 22:04:43 +0530

    #9 bugs fixes done for file app and logics hasbeen relocate accordingly

--- fileapp/admin.py
+++ fileapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import FileDetail, FileUpload
 # Register your models here.
+class FileAdmin(admin.StackedInline):
+    model = FileUpload
+
+
+@admin.register(FileDetail)
+class DetailAdmin(admin.ModelAdmin):
+    inlines = [FileAdmin]
+
+
+@admin.register(FileUpload)
+class UploadAdmin(admin.ModelAdmin):
+    list_per_page = 30
