f250d79beeed6171942e8a3a72406afac4a4e9e5
RajrupDasid <rajrupdasid@keemail.me>
2023-05-29 22:05:37 +0530

    #9 microarc logics hasbeen moved to a new location as needed

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,20 +1,5 @@
 from django.contrib import admin
-from .models import (FileUpload, FileDetail, Photo, PhotoDetails, VideoFileDetails, VideoUpload)
-
-
-class FileAdmin(admin.StackedInline):
-    model = FileUpload
-
-
-@admin.register(FileDetail)
-class DetailAdmin(admin.ModelAdmin):
-    inlines = [FileAdmin]
-
-
-@admin.register(FileUpload)
-class UploadAdmin(admin.ModelAdmin):
-    list_per_page = 30
-
+from .models import (Photo, PhotoDetails)
 
 class PhotoAdmin(admin.StackedInline):
     model = Photo
@@ -27,23 +12,4 @@ class PhotoDetailAdmin(admin.ModelAdmin):
 
 @admin.register(Photo)
 class PhotoUploadAdmin(admin.ModelAdmin):
-    list_per_page = 30
-
-
-class VideoAdmin(admin.StackedInline):
-    model = VideoUpload
-    exclude = ["slug",]
-
-
-@admin.register(VideoFileDetails)
-class VideoDetailsAdmin(admin.ModelAdmin):
-    inlines = [VideoAdmin]
-    list_display = ('name', 'created',)
-    list_display_links = ('name',)
-    list_per_page = 30
-
-
-@admin.register(VideoUpload)
-class VideoUploadAdmin(admin.ModelAdmin):
-    exclude = ["slug",]
     list_per_page = 30
