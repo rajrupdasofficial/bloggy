6a5ca55279dbd2ea1bc2ba584645901271555421
RajrupDasid <rajrupofficialid@protonmail.com>
2023-06-07 11:37:40 +0530

    #5 namespace added for better identification

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import index, blogdetail, galleryview, contactview, aboutview, watchview, watch_all, searchview
 
+
+app_name = 'app'
 urlpatterns = [
     path("", index, name="indexpage"),
     path("<slug:slug>.html", blogdetail, name="article-detail"),
