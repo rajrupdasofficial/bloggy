dd3993c06034bf789a4ae395dd901e125f5130e9
RajrupDasid <rajrupofficialid@protonmail.com>
2023-06-11 17:29:05 +0530

    #5 logout url implemented

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from account.views import Registration,Login
+from account.views import Registration,Login,Logout
 
 
 app_name = 'account'
@@ -7,4 +7,5 @@ app_name = 'account'
 urlpatterns = [
     path('signup',Registration.as_view(),name='signup'),
     path('login',Login.as_view(),name='login'),
+    path('logout',Logout.as_view(),name='logout'),
 ]
