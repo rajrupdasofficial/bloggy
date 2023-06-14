e069212052f19ddb71ec6db6b7f5fa30a8aa4027
RajrupDasid <rajrupofficialid@protonmail.com>
2023-06-14 20:05:34 +0530

    #5 profile  api hasbeen made

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import UserProfile
+from .serializers import UserProfileSerializer
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class ProfileAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return render(request, 'profileapp/profile.html')
+
+class ProfileAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user_profile = UserProfile.objects.get(user=request.user)
+        serializer = UserProfileSerializer(user_profile)
+        return Response(serializer.data)
+
