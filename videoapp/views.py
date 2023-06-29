a864bcbf8905b3e9948b38d937c0e4078c61ecb5
RajrupDasid <rajrupofficialid@protonmail.com>
2023-06-29 18:37:57 +0530

    implemented easy cache
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- videoapp/views.py
+++ videoapp/views.py
@@ -4,13 +4,18 @@ from rest_framework.response import Response
 from .models import VideoUpload
 from .serializers import VideoUploadSerializer
 from rest_framework import permissions
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from django.conf import settings
 
 # Create your views here.
 
-
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 class IndexAPIView(APIView):
     permission_classes = (permissions.AllowAny, )
+    @method_decorator(cache_page(CACHE_TTL))
     def get(self, request):
         all_videos = VideoUpload.objects.all().order_by('-created')
         serializer = VideoUploadSerializer(all_videos, many=True)
