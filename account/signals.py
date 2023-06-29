40002a5cf671e813395c0e27f09569aa86603c90
RajrupDasid <rajrupofficialid@protonmail.com>
2023-06-29 18:29:39 +0530

    removed print statements
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- account/signals.py
+++ account/signals.py
@@ -4,10 +4,7 @@ from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-print(User)
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
-    print("def section working")
     if created:
-        print("if created working")
         UserProfile.objects.create(user=instance)
