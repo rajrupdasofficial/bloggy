3bed0a6fa8d4ca26b95ebdb98e898dc4364dd590
RajrupDasid <rajrupofficialid@protonmail.com>
2023-06-29 18:37:10 +0530

    picture saving logic has reconfigured
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- profileapp/models.py
+++ profileapp/models.py
@@ -7,29 +7,35 @@ from PIL import Image as Img
 from django.core.files.uploadedfile import InMemoryUploadedFile
 import random
 import string
-piuf = uuid.uuid4()
+import os
+from django.utils.crypto import get_random_string
+
+
 
 def random_string_generator(size=100, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
-def random_uuid():
-    return uuid.uuid4()
+
+
+def random_gen(size=35):
+    return ''.join(get_random_string(size))
+
+
+def userprofile_image_upload_location(instance, imagename):
+        rand_chars = get_random_string(20)
+        image_file_area = rand_chars
+        random_image_name = get_random_string(30)
+        image_name = f"{random_image_name}_{imagename.lower().replace('-', '')}"
+        return os.path.join(str(uuid.uuid4()), image_file_area, image_name)
+
+
 
 class UserProfile(models.Model):
     user=models.OneToOneField(settings.AUTH_USER_MODEL, related_name='author' , on_delete=models.CASCADE)
-    profileimage = models.ImageField(upload_to=random_uuid(),default=None, blank=True,null=True)
+    profileimage = models.ImageField(upload_to=userprofile_image_upload_location,default=None, blank=True,null=True)
     shortbio=models.TextField(blank=True,null=True,default=None)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     
-    def save(self, *args, **kwargs):
-        if self.profileimage:
-            img = Img.open(BytesIO(self.profileimage.read()))
-            if img.mode != 'RGB':
-                img = img.convert('RGB')
-            img.thumbnail((self.profileimage.width / 1.5, self.profileimage.height / 1.5), Img.ANTIALIAS)
-            output = BytesIO()
-            img.save(output, format='WebP', quality=60)
-            output.seek(0)
-            self.thumbnail = InMemoryUploadedFile(output, 'ImageField', "%s.webp" % self.profileimage.name.join(
-                random_string_generator()).split('.')[0:10], 'profileimage/webp', len(output.getbuffer()), None)
-        super().save(*args, **kwargs)
+    
+
+
