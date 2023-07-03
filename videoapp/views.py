0368bf5b3ad6e288e73b8ed066e5564f6de17db6
RajrupDasid <rajrupofficialid@protonmail.com>
2023-07-03 22:06:00 +0530

    secured API video watch logic added
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- videoapp/views.py
+++ videoapp/views.py
@@ -1,12 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import VideoUpload
-from .serializers import VideoUploadSerializer
+from .serializers import VideoUploadSerializer,VideoPlayerSerializer
 from rest_framework import permissions
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from rest_framework_api_key.permissions import HasAPIKey
 
 # Create your views here.
 
@@ -14,7 +16,7 @@ CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 
 class IndexAPIView(APIView):
-    permission_classes = (permissions.AllowAny, )
+    permission_classes = [HasAPIKey]
 
     @method_decorator(cache_page(CACHE_TTL))
     def get(self, request):
@@ -24,3 +26,16 @@ class IndexAPIView(APIView):
 
     def post(self, request):
         return Response({'error': 'Something went wrong. Please try again.'}, status=400)
+
+
+class VideoDetailView(APIView):
+    permission_classes = [HasAPIKey]
+
+    @method_decorator(cache_page(CACHE_TTL))
+    def get(request,vid):
+        video_slug = get_object_or_404(VideoUpload, vid=vid)
+        serializer = VideoPlayerSerializer(video_slug, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        return Response({'error': 'Something went wrong. Please Try again'}, status=400)
