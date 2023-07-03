b6f560df2fe4bdd8fcd1a05a358f666b68cf8ef5
RajrupDasid <rajrupofficialid@protonmail.com>
2023-07-03 22:05:18 +0530

    video wplayer view
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- videoapp/urls.py
+++ videoapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexAPIView
+from .views import IndexAPIView, VideoDetailView
 app_name = "videoapp"
 urlpatterns = [
     path('', IndexAPIView.as_view(), name="indexpage"),
+    path('watch/v/<str:vid>',VideoDetailView.as_view(),name='detailvideo'),
 ]
